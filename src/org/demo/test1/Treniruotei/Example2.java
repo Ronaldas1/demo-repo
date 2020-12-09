2987a3f2525fb19de158fda0f9089bd360d3fbb4
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2020-12-09 12:11:36 +0200

    004 pamoka uzd ar paduotas skaicius lyginis ir Bridges

---
+++ src/org/demo/test1/Treniruotei/Example2.java
@@ -0,0 +1,46 @@
+package org.demo.test1.Treniruotei;
+
+//Turim metodą, kuris patikrina ar paduotas skaičius yra lyginis. Tarkim, prireikė turėti metodą arNelyginis().
+class Main {
+    public static void main(String[] args) {
+
+
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);
+        System.out.println(x % 2 == 0);
+    }
+
+    public class Bridges {
+        public boolean getway(
+            boolean p1p2 = true;
+            boolean p1p3 = true;
+            boolean p2p3 = true;
+            boolean p3p5 = false;
+            boolean p3p4 = true;
+            boolean p3ps = false;
+            boolean p2P5 = false;
+            boolean p4p5 = false;
+            boolean p4p6 = true;
+            boolean p6p5 = true;
+            boolean Ap1 = true;
+            boolean Ap4 = true;
+            boolean p2B = true;
+            boolean p5B = true){
+            return Ap1 && p1p2 && p2B == true;
+            }
+        }
+
+
+
+
+    }
+    //int c = 6;
+    //private boolean arNelyginis  (int sk){
+    //int c = 6;
+    //boolean c = true;
+    //boolean arNelyginis = c % 2 == 0;
+    //System.out.println(c / 2 ==0);
+    //return c % 2 == 0;
+}
+
