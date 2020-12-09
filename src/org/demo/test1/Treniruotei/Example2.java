565429334afeb8aea4366392b024c6ef5fc72cf9
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2020-12-09 13:01:45 +0200

    004 pam.uzd. Bridges kitu budu

--- src/org/demo/test1/Treniruotei/Example2.java
+++ src/org/demo/test1/Treniruotei/Example2.java
@@ -3,38 +3,42 @@ package org.demo.test1.Treniruotei;
 //Turim metodą, kuris patikrina ar paduotas skaičius yra lyginis. Tarkim, prireikė turėti metodą arNelyginis().
 class Main {
     public static void main(String[] args) {
+        boolean p1p2 = true;
+        boolean p1p3 = true;
+        boolean p2B = true;
+        boolean Ap1 = true;
+        boolean Ap4 = true;
+        boolean p3p5 = false;
+        boolean p4p5 = false;
+        boolean p5B = true;
+        System.out.println(Ap1 == p1p2 == p2B );
+        System.out.println(Ap1 == p1p3 == p3p5 == p5B);
+        //tokiu budu sudeciau visa masruta is A per p iki B.
 
-
-        int x = 10;
-        int y = 9;
-        System.out.println(x > y);
-        System.out.println(x % 2 == 0);
     }
 
+
+        //int x = 10;
+        //int y = 9;
+        //System.out.println(x > y);
+        //System.out.println(x % 2 == 0);
+    //}
+
     public class Bridges {
-        public boolean getway(
-            boolean p1p2 = true;
-            boolean p1p3 = true;
-            boolean p2p3 = true;
-            boolean p3p5 = false;
-            boolean p3p4 = true;
-            boolean p3ps = false;
-            boolean p2P5 = false;
-            boolean p4p5 = false;
-            boolean p4p6 = true;
-            boolean p6p5 = true;
-            boolean Ap1 = true;
-            boolean Ap4 = true;
-            boolean p2B = true;
-            boolean p5B = true){
-            return Ap1 && p1p2 && p2B == true;
+        //public boolean getway(
+            //boolean p1p2 = true;
+            //boolean p1p2 = true;
+
+
+        //return Ap1 && p1p2 && p2B == true;
             }
         }
 
 
 
 
-    }
+
+    //}
     //int c = 6;
     //private boolean arNelyginis  (int sk){
     //int c = 6;
@@ -42,5 +46,5 @@ class Main {
     //boolean arNelyginis = c % 2 == 0;
     //System.out.println(c / 2 ==0);
     //return c % 2 == 0;
-}
+//}
 
