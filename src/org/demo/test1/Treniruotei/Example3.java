b9c74c9e7be1914d3cc829a26583721666dd99d4
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2020-12-09 14:08:21 +0200

    04 pamoka uzd Sultys 1 dalis

---
+++ src/org/demo/test1/Treniruotei/Example3.java
@@ -0,0 +1,18 @@
+package org.demo.test1.Treniruotei;
+//04 pamoka uzd Sultys
+public class Example3 {
+    public class Sultys {
+        int sulciuKiekisViso = 10;
+        int talposTuris = 3;
+        boolean reikalingasTikLyginis = true;
+
+        public int arNeraLikucio(int sulciuKiekisViso,
+                                 int talposTuris,
+                                 boolean reikalingasTikLyginis) {
+
+            //System.out.println(sulciuKiekisViso); talposTuris;);
+            return sulciuKiekisViso / talposTuris;
+            //return reikalingasTikLyginis;
+        }
+    }
+}
