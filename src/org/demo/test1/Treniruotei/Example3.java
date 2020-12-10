802e22e473900f5be931e9ed53319076ea6c2f00
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2020-12-10 19:16:28 +0200

    laipau ant grebliu

--- src/org/demo/test1/Treniruotei/Example3.java
+++ src/org/demo/test1/Treniruotei/Example3.java
@@ -1,18 +1,41 @@
 package org.demo.test1.Treniruotei;
 //04 pamoka uzd Sultys
 public class Example3 {
-    public class Sultys {
+    public class Main {
         int sulciuKiekisViso = 10;
         int talposTuris = 3;
+        int arNeraLikucio = 0;
         boolean reikalingasTikLyginis = true;
 
-        public int arNeraLikucio(int sulciuKiekisViso,
-                                 int talposTuris,
-                                 boolean reikalingasTikLyginis) {
+        //public int arNeraLikucio(int sulciuKiekisViso,
+                                 //int talposTuris,
+                                 //boolean reikalingasTikLyginis) {
 
             //System.out.println(sulciuKiekisViso); talposTuris;);
-            return sulciuKiekisViso / talposTuris;
+            //return sulciuKiekisViso / talposTuris;
             //return reikalingasTikLyginis;
+            //return sulciuKiekisViso / talposTuris;
+
+        //}
+        private boolean Sultys ( int sulciuKiekisViso,
+                                 int talposTuris,
+                                 boolean reikalingasTikLyginis) {
+           //return sulciuKiekisViso % talposTuris == 3;
+           return sulciuKiekisViso / talposTuris == 0;
+        }
+        private boolean arLyginis ( int sulciuKiekisViso) {
+            return sulciuKiekisViso / 3 == 0;
+        }
+        private boolean Sultys1 ( int sulciuKiekisViso,
+                                  int talposTuris) {
+            return sulciuKiekisViso % talposTuris == 0;
+        }
+        private boolean Sultys2 (int sulciuKiekisViso,
+                                 int talposTuris,
+                                 boolean reikalingasTikLyginis,
+                                 int arNeraLikucio){
+            return sulciuKiekisViso / talposTuris == arNeraLikucio;
+        }
         }
     }
-}
+
---
+++ src/org/demo/test1/Treniruotei/Example4.java
@@ -0,0 +1,42 @@
+package org.demo.test1.Treniruotei;
+
+public class Example4 {
+    public static void main(String[] args) {
+
+        int sulciuKiekisViso = 10;
+        int talposTuris = 3;
+        int arNeraLikucio = 0;
+        boolean reikalingasTikLyginis = true;
+    }
+
+    private boolean Sultys(int sulciuKiekisViso,
+                           int talposTuris,
+                           boolean reikalingasTikLyginis) {
+        //return sulciuKiekisViso % talposTuris == 3;
+        //return sulciuKiekisViso / talposTuris == 0;
+        System.out.println(sulciuKiekisViso / talposTuris == 0);
+        return reikalingasTikLyginis;
+    }
+
+    private int arLyginis(int sulciuKiekisViso,
+                          int talposTuris) {
+        //return sulciuKiekisViso / 3 == 0;
+        System.out.println(sulciuKiekisViso / talposTuris == 0);
+        return 10 / 3;
+
+    }
+
+    private void Sultys1(int sulciuKiekisViso,
+                         int talposTuris) {
+        //return sulciuKiekisViso % talposTuris == 0;
+        System.out.println(sulciuKiekisViso % talposTuris == 0);
+    }
+
+    private void Sultys2(int sulciuKiekisViso,
+                         int talposTuris,
+                         boolean reikalingasTikLyginis,
+                         int arNeraLikucio) {
+        //return sulciuKiekisViso / talposTuris == arNeraLikucio;
+        System.out.println(sulciuKiekisViso / talposTuris == arNeraLikucio);
+    }
+}
