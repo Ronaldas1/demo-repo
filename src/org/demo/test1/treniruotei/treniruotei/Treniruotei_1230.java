7028403e6725bf9bd2efb86938b42f1c517307dd
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2020-12-30 19:23:34 +0200

    package projectVGym/VGym_1; package treniruotei/Treniruotei_1230

---
+++ src/org/demo/test1/treniruotei/treniruotei/Treniruotei_1230.java
@@ -0,0 +1,14 @@
+package org.demo.test1.treniruotei.treniruotei;
+
+import org.demo.test1.treniruotei.TaskPersonInitials;
+
+public class Treniruotei_1230 {
+    public static void main(String[] args) {
+        TaskPersonInitials lastCharacter = new TaskPersonInitials();
+        String rezultatas = lastCharacter.makeInitials("Jonas");
+        System.out.println(rezultatas.charAt(4));
+
+    }
+
+
+}
---
+++ src/projectVGym/VGym_1.java
@@ -0,0 +1,129 @@
+package projectVGym;
+
+public class VGym_1 {
+    private String yoga;
+    private String taichi;
+    private String pilates;
+    private String dancing;
+    private String walking;
+    private String running;
+    private String cycling;
+    private String swimming;
+    private String rowing;
+    private String sportGamesInd;
+    private String sportGamesTeam;
+    private String martialArts;
+    private String liftingWeights;
+    private String modernWorkouts;
+
+    public String getYoga() {        return yoga;
+    }
+
+    public void setYoga(String yoga) {
+        this.yoga = yoga;
+    }
+
+    public String getTaichi() {
+        return taichi;
+    }
+
+    public void setTaichi(String taichi) {
+        this.taichi = taichi;
+    }
+
+    public String getPilates() {
+        return pilates;
+    }
+
+    public void setPilates(String pilates) {
+        this.pilates = pilates;
+    }
+
+    public String getDancing() {
+        return dancing;
+    }
+
+    public void setDancing(String dancing) {
+        this.dancing = dancing;
+    }
+
+    public String getWalking() {
+        return walking;
+    }
+
+    public void setWalking(String walking) {
+        this.walking = walking;
+    }
+
+    public String getRunning() {
+        return running;
+    }
+
+    public void setRunning(String running) {
+        this.running = running;
+    }
+
+    public String getCycling() {
+        return cycling;
+    }
+
+    public void setCycling(String cycling) {
+        this.cycling = cycling;
+    }
+
+    public String getSwimming() {
+        return swimming;
+    }
+
+    public void setSwimming(String swimming) {
+        this.swimming = swimming;
+    }
+
+    public String getRowing() {
+        return rowing;
+    }
+
+    public void setRowing(String rowing) {
+        this.rowing = rowing;
+    }
+
+    public String getSportGamesInd() {
+        return sportGamesInd;
+    }
+
+    public void setSportGamesInd(String sportGamesInd) {
+        this.sportGamesInd = sportGamesInd;
+    }
+
+    public String getSportGamesTeam() {
+        return sportGamesTeam;
+    }
+
+    public void setSportGamesTeam(String sportGamesTeam) {
+        this.sportGamesTeam = sportGamesTeam;
+    }
+
+    public String getMartialArts() {
+        return martialArts;
+    }
+
+    public void setMartialArts(String martialArts) {
+        this.martialArts = martialArts;
+    }
+
+    public String getLiftingWeights() {
+        return liftingWeights;
+    }
+
+    public void setLiftingWeights(String liftingWeights) {
+        this.liftingWeights = liftingWeights;
+    }
+
+    public String getModernWorkouts() {
+        return modernWorkouts;
+    }
+
+    public void setModernWorkouts(String modernWorkouts) {
+        this.modernWorkouts = modernWorkouts;
+    }
+}
