f9f2dc02708ae5d73e3d37ad7e43ef39376797b7
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-01-07 21:53:06 +0200

    task Tiltai

--- src/bridge/Bridge.java
+++ src/bridge/Bridge.java
@@ -2,8 +2,22 @@ package bridge;
 
 public class Bridge {
 
-	public boolean getWay(boolean p1, boolean p2, boolean p3, boolean p4, boolean p5, boolean p6) {
+	public void getWay() {
 
-		return true;
+		boolean p1 = true;
+		boolean p2 = true;
+		boolean p3 = false;
+		boolean p4 = true;
+		boolean p5 = true;
+		boolean p6 = false;
+		// return p1 && p2;
+		// return p4 && p5;
+		// return p4 && p6 && p5;
+		// return p1 && p3 && p5;
+		System.out.println(p1 && p2);
+		System.out.println(p4 && p5);
+		System.out.println(p4 && p6 && p5);
+		System.out.println(p1 && p3 && p5);
+
 	}
 }
