5c55527bab4d1063968c8d59c920344f26f63527
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-01-11 20:35:38 +0200

    Formule

---
+++ src/task/sav1/tipai/formule/Formule1.java
@@ -0,0 +1,36 @@
+package task.sav1.tipai.formule;
+
+public class Formule1 {
+	public int a;
+	public int d;
+	public int n;
+	public int s;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getD() {
+		return d;
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+	public int getN() {
+		return n;
+	}
+	public void setN(int n) {
+		this.n = n;
+	}
+	public int getS() {
+		return s;
+	}
+	public void setS(int s) {
+		this.s = s;
+	}
+	
+	
+	
+
+}
---
+++ src/task/sav1/tipai/formule/Formule2.java
@@ -0,0 +1,37 @@
+package task.sav1.tipai.formule;
+
+public class Formule2 {
+	Formule2 rezultatas = new Formule2();
+	
+	int a;
+
+	public int getA() {
+		return a = 10;
+	}
+
+	int n;
+
+	public int getN() {
+		return n = 15;
+	}
+
+	int d;
+
+	public int getD() {
+		return d;
+	}
+
+	int s;
+
+	public int getS() {
+		return s = x;
+
+	}
+	
+	int x = (((2 * a) + ( d * (n - 1))) / 2) * n;
+
+	System.out.@println(x);
+	
+	
+	}
+
