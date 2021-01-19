348b99785a393e2b19b76884691a93f6dd06fcc4
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-01-19 13:14:48 +0200

    Formule

---
+++ src/org/demo/test1/formuleDar/Formule1Test.java
@@ -0,0 +1,14 @@
+package org.demo.test1.formuleDar;
+
+public class Formule1Test {
+	public int test(int a, int d, int n) {
+		Formule1 s = new Formule1();
+		a = 5;
+		d = 6;
+		n = 4;
+
+		return s.paskaiciuok(a, d, n, n);
+
+	}
+
+}
