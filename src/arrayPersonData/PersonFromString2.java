91980188095fda3b7bf7bec93e7948bb6d6cd578
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-02-05 20:45:26 +0200

    PersonFromString2 Quiz Matrica

--- src/arrayPersonData/PersonFromString2.java
+++ src/arrayPersonData/PersonFromString2.java
@@ -2,14 +2,15 @@ package arrayPersonData;
 
 //import arrayPersonData.PersonFromString.Person;
 import arrayPersonData.PersonFromString1.Lytis;
+import arrayPersonData.PersonFromString1.Person;
 
 public class PersonFromString2 {
 
-	// public static void main(String[] args) {
+	 //public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	public class PersonData {
 
-		public Person personFromString(String person) {
+		public Person personFromString2(String person) {
 			Person persona = new Person();
 
 			persona.setId("4485465456");
@@ -19,14 +20,26 @@ public class PersonFromString2 {
 			persona.setGimMetai(1994);
 			// persona.setLytis("MOT");
 			Lytis.valueOf("MOT");
+			persona = personFromString2(person);
 
 			person = "id" + "vardas" + "pavarde" + "ak" + "gimMetai" + "lytis";
 			person.split(person);
-			System.out.println(person);
+			System.out.println(person.split(person));
 			Integer gimMetai1 = Integer.parseInt("gimMetai");
-			System.out.println();
+			System.out.println(Integer.parseInt("4485465456"+ "1994"));
+			System.out.println(Integer.parseInt("3485465456"+ "1984"));
+			//System.out.println(String.split(Ona, Onute, 4485465456, 1984, mot));
 			// person = Integer.parseInt("gimMetai");
-
+			class StringSplit{
+				  public void main(String []args){
+				   String persona = "";
+				    String[] arrSplit = persona.split("4485465456, Ona, Onute, 1994, MOT ");
+				    for (int i=0; i < arrSplit.length; i++)
+				    {
+				      System.out.println(arrSplit[i]);
+				    }
+				  }
+				}
 			return null;
 		}
 	}
