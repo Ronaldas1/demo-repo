d9fca0ac28bee08f33fe818717aee72394a17b51
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-03-30 22:25:31 +0300

    Task Average

--- src/arrayNamuDarbai/TaskAverage1_1.java
+++ src/arrayNamuDarbai/TaskAverage1_1.java
@@ -2,26 +2,40 @@ package arrayNamuDarbai;
 
 public class TaskAverage1_1 {
 	public double calcAverage(double[] data) {
-		//TODO ...
-		
-	double[] data1 = {};	
-	//double sum = 0;
-	//for (double value : data1 ) {
-		//sum += value;
-	return 0.0;
+		// TODO ...
+
+		//double[] data1 = {};
+		// double sum = 0;
+		// for (double value : data1 ) {
+		// sum += value;
+		//return 0.0;
+		double[] data1 =  {2.00, 3.00 };
+		double sum = 0;
+		for (double value : data1) {
+			sum += value;
+			//System.out.println(sum += value);
+		}
+		return sum / data1.length;
+		 
+		//System.out.println(sum);
+		//System.out.println();
 	}
-	
- //return sum;
+
+	// return sum;
 
 	public static void main(String[] args) {
-		double[] data1 = new double[] { 1.11, 2.15, 3.25, 16.99};
-		double sum = 0;
-		for (double value : data1 ) {
-			sum += value;
-		}
+		TaskAverage1_1 ta1_1 = new TaskAverage1_1(); 
+		double[] data1 = new double[] { };
+		//double sum = 0;
+		//for (double value : data1) {
+			//sum += value;
+		//}
+
+		//System.out.println(sum);
+		//ta1_1.calcAverage(data1);
 		
-	 System.out.println(sum);
-		
+		System.out.println(ta1_1.calcAverage(data1));
+
 	}
-	
+
 }
--- src/arrayNamuDarbai/TaskAverage1_2.java
+++ src/arrayNamuDarbai/TaskAverage1_2.java
@@ -5,12 +5,19 @@ import java.lang.reflect.Array;
 public class TaskAverage1_2 {
 	public double calcAverage(double[] data) {
 		// TODO ...
-		double[] data1 = { 1.25, 3.16, 15.898 };
-		double sum = Array.getDouble(data1, 0);
-		return (double) sum / data1.length;
+		double[] data1 = { };
+		double sum = 0;
+		for (double i : data1)
+			sum += i;
+		double size = data1.length;
+		double average = sum / size;
+		//double sum = Array.getDouble(data1, 0);
+		//return (double) sum / data1.length;
+		return 0;
 	}
 
 	public static void main(String[] args) {
+		TaskAverage1_2 ta1_2 = new TaskAverage1_2();
 		double[] data1 = new double[] { 1.25, 3.16, 15.898 };
 		double sum = 0;
 		for (double i : data1)
