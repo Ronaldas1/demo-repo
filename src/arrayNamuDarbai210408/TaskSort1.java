e4b51d5e50eba8afaa8bf22af0c527cdc334df5e
Ronaldas Endrijaitis <ronaldas.endrijaitis@gmail.com>
2021-04-08 22:00:55 +0300

    Task Sort 1 version

---
+++ src/arrayNamuDarbai210408/TaskSort1.java
@@ -0,0 +1,30 @@
+package arrayNamuDarbai210408;
+
+import java.util.Arrays;
+
+public class TaskSort1 {
+	public int[] sort(String data, boolean ascending) {
+		//TODO …"3242 43 22222 34 1111111 234 234"
+String data1 =  "3242 43 22222 34 1111111 234 234";
+String[] data2 = data1.split(" ");
+		return null;
+		
+		//int data2 = data1;
+		}
+
+public int dataToInt (String data2) {
+	//int i = Integer.parseInt(data2);
+	int[] data3 = new int[data2.length()];
+	for(int i =0; i<data2.length(); i++); 
+	//int[] data3 = Integer.parseInt(data2);
+	return Integer.parseInt(data2);
+}
+public static void main(String[] args) {
+	TaskSort1 y = new TaskSort1();
+	//System.out.println(Arrays.toString(data3));
+	//System.out.println(Arrays.toString(y));
+	System.out.println("3242  43  22222  34  1111111  234  234");
+	//System.out.println(Arrays.toString("3242  43  22222  34  1111111  234  234"));
+}
+}
+
